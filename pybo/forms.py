8e3ed85649444b36c27a3e85d3591f1c068cb356
우코코 <pyboo@gmail.com>
2021-06-21 18:01:10 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from pybo.models import Question, Answer, Comment
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+
+
+
+        """
+            label 속성 수정 Subject와 Content 한글로 변경
+        """
+        labels = {
+            'subject': '제목!',
+            'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content' : '답변내용',
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': '댓글내용',
+        }
